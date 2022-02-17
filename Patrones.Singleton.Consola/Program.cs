2f8205d7a47a65afe9a3a5cd1b9e25a933ffff43
Adriel De La Cruz <adrielenmanuel11@gmail.com>
2022-02-17 17:11:37 -0400

    Add project files.

---
+++ Patrones.Singleton.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using Patrones.Singleton.Ejemplo1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Singleton.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User usuario1 = new User();
+            usuario1.Name = "Nancy";
+            
+           
+            try
+            {
+                SessionManager1.LogIn(usuario1);
+                SessionManager1 SM = SessionManager1.GetInstance;
+                SessionManager1.LogOut();
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+            
+            }
+
+           
+        }
+    }
+}
---
+++ Patrones.Singleton.Ejemplo1/SessionManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Singleton.Ejemplo1
+{
+    public class SessionManager1
+    {
+        //Elementos del inicio de sesión
+        private static SessionManager1 session;
+        public User Usuario { get; set; }
+     
+        public DateTime FechaInicio { get; set; }
+        
+        
+        //Función para obtener/devolver Instancias
+        public static SessionManager1 GetInstance
+        {
+            get
+            {
+                if (session == null) throw new Exception("Aún no se ha iniciado sesión");
+                return session;
+                
+            }
+
+        }
+
+       
+
+        private SessionManager1()
+        {
+        }
+
+        
+        //Métodos
+        public static object _lock = new object();
+        public static void LogIn(User pusuario)
+        {
+            lock (_lock)
+            {
+                              
+                    
+                if (session == null){
+                session = new SessionManager1();
+                session.Usuario = pusuario;
+                session.FechaInicio = DateTime.Now;
+                
+            
+                }
+                else
+                {
+                    throw new Exception("Sesión iniciada");
+                    
+                 }
+            }
+        }
+        public static void LogOut()
+        {
+            if (session != null)
+            {
+                session = null;
+                Console.WriteLine("Cerrar Sesión");
+            }
+        
+                
+           
+        }
+    }
+}
